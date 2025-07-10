f7a80cf37da756cc7d1e54ff139ac915a269923e
Abhishek Kumar <131546128+iamabhi0619@users.noreply.github.com>
2025-07-10 19:59:18 +0530

    a Person object using a constructor function with name and age properties.

---
+++ question/q11.js
@@ -0,0 +1,17 @@
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+const person1 = new Person("Abhi", 25);
+const person2 = new Person("Siya", 30);
+const person3 = new Person("Rohan", 22);
+
+console.log(person1.name);
+console.log(person1.age);
+
+console.log(person2.name);
+console.log(person2.age);
+
+console.log(person3.name);
+console.log(person3.age);
